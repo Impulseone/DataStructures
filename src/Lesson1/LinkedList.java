4d0cd0d344f78f0e9b336fe862a3e2a293ec964f
Sergei Krainyukov <sk-new@mail.ru>
2022-05-31 15:27:30 +0300

    Lesson1

---
+++ src/Lesson1/LinkedList.java
@@ -0,0 +1,100 @@
+public class LinkedList {
+    public Node head;
+    public Node tail;
+
+    public LinkedList() {
+        head = null;
+        tail = null;
+    }
+
+    public void addInTail(Node item) {
+        if (this.head == null)
+            this.head = item;
+        else
+            this.tail.next = item;
+        this.tail = item;
+    }
+
+    public Node find(int value) {
+        Node node = this.head;
+        while (node != null) {
+            if (node.value == value)
+                return node;
+            node = node.next;
+        }
+        return null;
+    }
+
+    public ArrayList<Node> findAll(int _value) {
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        Node node = this.head;
+        while (node != null) {
+            if (node.value == _value) nodes.add(node);
+            node = node.next;
+        }
+        return nodes;
+    }
+
+    public boolean remove(int _value) {
+        Node node = this.head;
+        while (node != null) {
+            if (this.head.value == _value) this.head = node.next;
+            if (node.next != null && node.next.value == _value) {
+                node.next = node.next.next;
+                return true;
+            }
+            node = node.next;
+        }
+        return false;
+    }
+
+    public void removeAll(int _value) {
+        Node node = this.head;
+        while (node != null) {
+            remove(_value);
+            node = node.next;
+        }
+    }
+
+    public void clear() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public int count() {
+        int result = 0;
+        Node node = this.head;
+        while (node != null) {
+            result += 1;
+            node = node.next;
+        }
+        return result;
+    }
+
+    public void insertAfter(Node _nodeAfter, Node _nodeToInsert) {
+        if (_nodeAfter == null) {
+            this.head = _nodeToInsert;
+            return;
+        }
+        Node node = this.head;
+        while (node != null) {
+            if (node.value == _nodeAfter.value) {
+                Node nextNode = node.next;
+                node.next = _nodeToInsert;
+                _nodeToInsert.next = nextNode;
+            }
+            node = node.next;
+        }
+    }
+
+}
+
+class Node {
+    public int value;
+    public Node next;
+
+    public Node(int _value) {
+        value = _value;
+        next = null;
+    }
+}
---
+++ src/Lesson1/LinkedListTest.java
@@ -0,0 +1,157 @@
+package Lesson1;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class LinkedListTest {
+    @Test
+    public void addInTailTest() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(10));
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(30));
+        list.addInTail(new Node(40));
+        Node resultNode = list.find(10);
+        assert resultNode.value == 10;
+    }
+
+    @Test
+    public void countTest1() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(10));
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(30));
+        list.addInTail(new Node(40));
+        assert list.count() == 4;
+    }
+
+    @Test
+    public void countTest2() {
+        LinkedList list = new LinkedList();
+        assert list.count() == 0;
+    }
+
+    @Test
+    public void countTest3() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(10));
+        assert list.count() == 1;
+    }
+
+    @Test
+    public void clearTest() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(10));
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(30));
+        list.clear();
+        assert list.count() == 0;
+    }
+
+    @Test
+    public void removeTest1() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(10));
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(20));
+        list.remove(20);
+        assert list.count() == 2;
+    }
+
+    @Test
+    public void removeTest2() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(10));
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(30));
+        list.remove(50);
+        assert list.count() == 3;
+    }
+
+    @Test
+    public void removeTest3() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(10));
+        list.addInTail(new Node(20));
+        list.remove(10);
+        assert list.count() == 1;
+    }
+
+    @Test
+    public void removeAllTest() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(10));
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(20));
+        list.removeAll(20);
+        assert list.count() == 1;
+    }
+
+    @Test
+    public void removeAllTest2() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(10));
+        list.removeAll(20);
+        assert list.count() == 1;
+    }
+
+    @Test
+    public void findAllTest1() {
+        LinkedList list = new LinkedList();
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(20));
+        list.addInTail(new Node(10));
+        ArrayList<Node> nodes = list.findAll(20);
+        assert nodes.size() == 2;
+    }
+
+    @Test
+    public void insertTest1() {
+        LinkedList list = new LinkedList();
+        Node node = new Node(20);
+        list.addInTail(node);
+        list.addInTail(new Node(50));
+        list.insertAfter(node, new Node(40));
+        assert list.count() == 3;
+    }
+
+    @Test
+    public void insertTest2() {
+        LinkedList list = new LinkedList();
+        list.insertAfter(null, new Node(40));
+        assert list.count() == 1;
+    }
+
+    @Test
+    public void fun() {
+        LinkedList list1 = new LinkedList();
+        list1.addInTail(new Node(10));
+        list1.addInTail(new Node(20));
+
+        LinkedList list2 = new LinkedList();
+        list2.addInTail(new Node(30));
+        list2.addInTail(new Node(40));
+
+        LinkedList list3 = new LinkedList();
+        if (list1.count() == list2.count()) {
+            Node node1 = list1.head;
+            Node node2 = list2.head;
+            for (int i = 0; i < list1.count(); i++) {
+                if (node1 != null) {
+                    int sum = node1.value + node2.value;
+                    list3.addInTail(new Node(sum));
+                    node1 = node1.next;
+                    node2 = node2.next;
+                }
+            }
+        }
+
+        assert list3.count() == 2;
+        assert list3.head.value == 40;
+        assert list3.tail.value == 60;
+
+    }
+}
